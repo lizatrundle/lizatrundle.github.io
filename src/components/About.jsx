f121735277cf09e5ec82e021e05d221359f0d73b
Liza Trundle <liza.trundle@fticonsulting.com>
2025-12-21 17:41:46 -0500

    update resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ function About() {
         <p className="intro">
           Hi! My name is Elizabeth (Liza) Trundle and I am a software developer currently working in New York City. I graduated from the University of Virginia in 2024 with a degree in Computer Science and Data Science. I bring creativity and dedication to solving complex technical challenges. Originally from Charlottesville, Virginia, I'm passionate about leveraging technology to build innovative solutions. Please feel free to reach out—I'd love to connect!
         </p>
-        <a href="/resume.pdf" download="resume.pdf" className="resume-link">
+        <a href="/Resume2025.pdf" download="Liza_Trundle_Resume_2025.pdf" className="resume-link">
           Download My Resume →
         </a>
       </div>
